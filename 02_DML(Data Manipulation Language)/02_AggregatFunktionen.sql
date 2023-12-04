3b7bd100458d8728ebbfb03c16112e8f992ebe91
Adrian Weidig <70036819+adrianweidig@users.noreply.github.com>
2023-12-04 11:27:52 +0100

    Create 02_AggregatFunktionen.sql

---
+++ 02_DML(Data Manipulation Language)/02_AggregatFunktionen.sql
@@ -0,0 +1,25 @@
+-- Verfügbare Aggregatfunktionen:
+/*
++-------------------+---------------------------------------------------------------+
+| Schlüsselwort     | Funktionalität                                                |
++-------------------+---------------------------------------------------------------+
+| COUNT             | Zählt die Anzahl der Zeilen in einer Ergebnismenge            |
+| SUM               | Summiert die Werte einer Spalte                               |
+| AVG               | Berechnet den Durchschnitt der Werte einer Spalte             |
+| MAX               | Gibt den maximalen Wert in einer Spalte zurück                |
+| MIN               | Gibt den minimalen Wert in einer Spalte zurück                |
+| FIRST             | Gibt den ersten Wert in einer Gruppe zurück                   |
+| LAST              | Gibt den letzten Wert in einer Gruppe zurück                  |
+| GROUP_CONCAT      | Konkateniert Zeichenketten aus einer Gruppe                   |
+| STDDEV            | Berechnet die Standardabweichung der Werte einer Spalte       |
+| VARIANCE          | Berechnet die Varianz der Werte einer Spalte                  |
+| UPPER             | Konvertiert einen String in Großbuchstaben                    |
+| LOWER             | Konvertiert einen String in Kleinbuchstaben                   |
+| LENGTH            | Gibt die Länge eines Strings zurück                           |
+| TRIM              | Entfernt Leerzeichen oder andere Zeichen vom Anfang und Ende  |
+| SUBSTRING         | Extrahiert einen Teil eines Strings                           |
+| CURRENT_DATE      | Gibt das aktuelle Datum zurück                                |
+| CURRENT_TIME      | Gibt die aktuelle Uhrzeit zurück                              |
+| CURRENT_TIMESTAMP | Gibt das aktuelle Datum und die aktuelle Uhrzeit zurück       |
++-------------------+---------------------------------------------------------------+
+*/
