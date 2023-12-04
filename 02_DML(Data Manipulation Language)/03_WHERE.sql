50e467938a465fe27557b8089edaa800a6e21598
Adrian Weidig <70036819+adrianweidig@users.noreply.github.com>
2023-12-04 14:20:57 +0100

    Update 03_WHERE.sql

--- 02_DML(Data Manipulation Language)/03_WHERE.sql
+++ 02_DML(Data Manipulation Language)/03_WHERE.sql
@@ -1,6 +1,31 @@
 -- NUTZEN DER DATENBANK UND TABELLE AUS 00_DatenbankUndTabellen.sql
 -- WHERE Klausel
 
+-- Beispiele ohne Aggregatfunktionen
 
+-- 1. SELECT mit WHERE-Klausel - Zeigt die Mitarbeiter an, die in Berlin arbeiten
+SELECT * FROM Mitarbeiter WHERE Ort = 'Berlin';
 
--- WHERE Klausel
+-- 2. SELECT mit ORDER BY - Zeigt die Mitarbeiter in absteigender Reihenfolge des Lebensalters an
+SELECT * FROM Mitarbeiter ORDER BY Lebensalter DESC;
+
+-- 3. SELECT mit LIMIT - Zeigt die ersten zwei Mitarbeiter an
+SELECT * FROM Mitarbeiter LIMIT 2;
+
+-- Beispiel: WHERE-Klauseln zu den Aggregatfunktionen
+
+-- 1. COUNT mit WHERE-Klausel - Zählt die Anzahl der Mitarbeiter, die älter als 30 Jahre sind
+SELECT COUNT(*) FROM Mitarbeiter WHERE Lebensalter > 30;
+
+-- 2. SUM mit WHERE-Klausel - Summiert die Lebensalter der Mitarbeiter, die in Berlin arbeiten
+SELECT SUM(Lebensalter) FROM Mitarbeiter WHERE Ort = 'Berlin';
+
+-- 3. AVG mit WHERE-Klausel - Berechnet den Durchschnitt des Lebensalters der Manager
+SELECT AVG(Lebensalter) FROM Mitarbeiter WHERE Beruf = 'Manager';
+
+-- 4. MAX mit WHERE-Klausel - Findet das maximale Lebensalter der Mitarbeiter in München
+SELECT MAX(Lebensalter) FROM Mitarbeiter WHERE Ort = 'München';
+
+-- 5. MIN mit WHERE-Klausel - Findet das minimale Lebensalter der Entwickler
+SELECT MIN(Lebensalter) FROM Mitarbeiter WHERE Beruf = 'Entwickler';
+
